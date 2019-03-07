8ab5358944de0e8b1884856414c5a347193dadd2
Juan Brown <juan.brown@hobsons.com>
2019-03-06 21:02:02 -0500

    dynamically creates eicar.com file for readyness check

--- readyness.sh
+++ readyness.sh
@@ -4,6 +4,9 @@ set -e
 
 if freshclam | grep -q 'bytecode.* is up to date'; then
   echo "freshclam running successfully"
+  echo "X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*" > /eicar.com
+  chown clamav: /eicar.com
+
   if clamdscan eicar.com | grep -q 'Infected files: 1'; then
     echo "Clamd running successfully"
     exit 0
